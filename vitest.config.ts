004381cf992c0fa8d3578764d8f736bd82811644
Aleksandr Kondratskii <aleksandr.kondratskii@macys.com>
2024-05-12 02:57:40 +0200

    test: ignore tests folders in code coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     globals: true,
     include: ['src/**/__tests__/*.test.ts'],
     coverage: {
-      exclude: ['**/index.ts'],
+      exclude: ['**/index.ts', '**/__tests__/**'],
     },
   },
 });
